6e29e84c4da8064c17f415220bea0eeaeaa9b495
takamajaca <takamajaca1989@gmail.com>
2022-11-17 11:11:21 -0300

    Primera tarea en rama

---
+++ js/index.js
@@ -0,0 +1,22 @@
+console.log("Buenos dias. Soy el codigo JS del archivo extereno html")
+
+console.log('Buenos dias. Soy el codigo JS del archivo externo del html')
+
+//Variables
+//Palabras reservadas
+var nombre;
+
+nombre = "Marcos"
+
+console.log ("Mi nombre es: " + nombre);
+
+let apellido = "Laass";
+console.log ("Mi apellido es: " + apellido)
+
+const provincia = "CABA";
+const DNI = 123456789;
+const PI=3.14;
+
+var nombre = "Pepe";
+apellido = "Sanchez";
+
